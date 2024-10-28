e5228fd652456c640a23bbe43b03ba99c227c43c
Nir Goren <nir@gorenor.com>
2024-10-28 17:25:36 +0200

    Use cuda Update environment.yml

--- normal_diffusion/training/training.py
+++ normal_diffusion/training/training.py
@@ -4,17 +4,23 @@ import torch.optim as optim
 
 from normal_diffusion.noise.gaussian_noise import gaussian_noise_loss
 
-def train_diffusion(model, dataloader, scheduler, n_epochs=100, lr=1e-3):
+def train_diffusion(model, dataloader, scheduler, n_epochs=100, lr=1e-3, writer=None, device='cpu'):
+    if writer is not None:
+        writer.add_text("model", str(model))
+        writer.add_text("scheduler", str(scheduler))
+        writer.add_text("n_epochs", str(n_epochs))
+        writer.add_text("lr", str(lr))
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     for epoch in range(n_epochs):
         total_loss = 0
-        for graph_data in dataloader:
+        for i, graph_data in enumerate(dataloader):
+            graph_data = graph_data.to(device=device)
             optimizer.zero_grad()
             batch_size = graph_data.size(0)
             noise = torch.randn_like(graph_data.x)
             clean_normals = graph_data.x
-            timesteps = torch.randint(0, scheduler.num_train_timesteps, (batch_size,))
+            timesteps = torch.randint(0, scheduler.num_train_timesteps, (batch_size,)).to(device=device)
             graph_data.x = scheduler.add_noise(graph_data.x, noise, timesteps)
             graph_data.x /= torch.norm(graph_data.x, dim=-1, keepdim=True)
             
@@ -24,6 +30,9 @@ def train_diffusion(model, dataloader, scheduler, n_epochs=100, lr=1e-3):
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
+            if writer is not None and i % 10 == 0:
+                writer.add_scalar("loss", loss.item(), epoch * len(dataloader) + i)
+            
         
-        if (epoch + 1) % 10 == 0:
-            print(f"Epoch {epoch+1}/{n_epochs}, Loss: {total_loss / len(dataloader):.4f}")
+        # if (epoch + 1) % 10 == 0:
+        print(f"Epoch {epoch+1}/{n_epochs}, Loss: {total_loss / (i+1):.4f}")
